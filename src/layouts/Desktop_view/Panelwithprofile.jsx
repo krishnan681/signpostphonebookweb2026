d1f0b9f27769760f094c207075c0f61cbefa818d
krishnan681 <gopalakrishnan0614@gmail.com>
2026-01-30 10:11:26 +0530

    thirdcommit

--- src/layouts/Desktop_view/Panelwithprofile.jsx
+++ src/layouts/Desktop_view/Panelwithprofile.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { supabase } from "../../services/supabaseClient";
 import { MdVerified } from "react-icons/md";
-import "./Desktop_css/Panelwithprofile.css";
+import "./Desktop_css/panelwithprofile.css";
 
 /* ─────────────────────────────
    Skeleton Card
